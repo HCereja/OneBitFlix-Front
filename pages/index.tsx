b87ebd2c1879aa8838c1508298e089fd89155c31
Henrique Costa Cereja <cerejahenrique@gmail.com>
2024-02-06 16:25:21 -0300

    Componente: Header da página homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
 
+import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
+
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>OneBitFlix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Plataforma EAD inspirada no Netflix"
+        ></meta>
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth />
+      </main>
     </>
   );
 };
