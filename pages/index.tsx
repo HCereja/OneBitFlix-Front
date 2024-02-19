fba13a4e2434de12424117068666188fe5900c09
Henrique Costa Cereja <cerejahenrique@gmail.com>
2024-02-19 15:39:33 -0300

    Componente de slide adicionado na página homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
+import { ReactNode } from "react";
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
-
 import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "../src/components/homeNoAuth/presentationSection";
 import CardsSection from "../src/components/homeNoAuth/cardSection";
+import SlideSection from "../src/components/homeNoAuth/slideSection";
+import { GetStaticProps } from "next";
+import courseService, { CourseType } from "../src/services/courseServices";
 
-const HomeNoAuth = () => {
+interface IndexPageProps {
+  children?: ReactNode;
+  course: CourseType[];
+}
+
+const HomeNoAuth = ({ course }: IndexPageProps) => {
   return (
     <>
       <Head>
@@ -23,9 +31,20 @@ const HomeNoAuth = () => {
           <PresentationSection />
         </div>
         <CardsSection />
+        <SlideSection newestCourses={course} />
       </main>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await courseService.getNewestCourses();
+  return {
+    props: {
+      course: res.data,
+    },
+    revalidate: 3600 * 24,
+  };
 };
 
 export default HomeNoAuth;
